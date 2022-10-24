ca1b87e218b5883c351abde9a94e8bcef90f0cd1
Emmanuel Ekpobimi <noble4special247@gmail.com>
2022-10-24 13:27:20 +0100

    Added QuickLinks component

---
+++ src/components/utilities/QuickLinks.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+
+function QuickLinks() {
+  return (
+    <Fragment>
+      <Container>
+        <h2>QuickLinks</h2>
+      </Container>
+    </Fragment>
+  );
+}
+
+export default QuickLinks;
