09123e0faca7ddf89dccd8c7f31bdf1ed8a1ed3e
Emmanuel Ekpobimi <noble4special247@gmail.com>
2022-10-29 19:48:08 +0100

    Created Proceeding.js Component

---
+++ src/components/utilities/Proceeding.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+
+function Proceeding() {
+  return (
+    <Fragment>
+      <Container>
+        <h1>Proceeding</h1>
+      </Container>
+    </Fragment>
+  );
+}
+
+export default Proceeding;
