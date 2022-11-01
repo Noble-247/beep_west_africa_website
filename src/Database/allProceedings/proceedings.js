969fdb6f82db3a29b61cf70b9ed3f7fbed002a8b
Emmanuel Ekpobimi <noble4special247@gmail.com>
2022-11-01 13:03:44 +0100

    Added allProceedings to the Database

---
+++ src/Database/allProceedings/proceedings.js
@@ -0,0 +1,165 @@
+const proceedings = [
+  {
+    id: 1,
+    title:
+      "11th Annual National Conference of the Association for Encouraging Quality Education in Nigeria",
+    topic:
+      "Concentration level of phosphate and nitrate in Ikpoba River of Edo State, Nigeria",
+    author: "Omoruyi C. I. and Oghuvwu P. O.",
+    articleNumber: "7E01D2",
+    abstract:
+      "Water is vital to the existence of all living organisms, but its value is increasingly being threatened as human populations grow and demand more water of high quality for various activities. This has led to water deterioration in quality and quantity for consumption by humans and has also impacted in the aquatic ecosystem. The aim of this paper is to access the vulnerability of Ikpoba River to eutrophication through the determination of phosphate and nitrate concentration. Eighteen water samples were collected from six stations in triplicate at an interval of two weeks throughout the duration of sampling. Temperature, pH, and conductivity were recorded in-situ and there was significant difference between nitrate and phosphate concentration together with majority of the other parameters recorded at the various stations. Comparison of results obtained from this study with those of other studies indicated that increased concentration of nitrate and phosphate in Ikpoba River are probably caused by discharge of pollution load from Guinness Nigerian Brewery’s domestic sewage, agriculture, and slaughter house which have further accelerated the occurrence of eutrophication.",
+    keywords: "Eutrophication, nutrients, phosphate, nitrate.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 2,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Assessment of rainwater quality of Bichi Local Government Area of Kano State, Nigeria",
+    author: "Emmanuel Bernard",
+    articleNumber: "5A3D279",
+    abstract:
+      "The physiochemical and bacteriological quality of rain water in Bichi Local Government Area of Kano State was studied using standard method of analysis. The result obtained indicated that the mean and standard deviation of temperature, pH, turbidity, total alkalinity, calcium, and magnesium were found to be 25.03 ± 0.82°C, 6.08 ± 0.34, 3.13 ± 0.64 NTU, 22.85 ± 6.45 mg/l, 5.76 ± 3.41 mg/l, 1.70 ± 0.65 mg/l, and 0.61± 0.00 mg/l, respectively. The value of other parameters such as chloride, free carbon dioxide, and conductivity was found to be 5.41±1.98 mg/l, 4.53 ± 2.27 mg/l, 27.71 ± 11.81 s/cm, respectively, while coli form and Escherichia coli were absent. It was observed that the rainwater samples as at collection were within WHO's standard. The study is significant in order to establish campaign and creating awareness for possible cautions to minimize environmental pollution that could contaminate rainwater and to reduce the incidence of water borne diseases in and around environment and Kano State in general.",
+    keywords:
+      "Assessment, bacteriological, Bichi, drinking, properties, rainwater, water.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 3,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Synthesis, characterization and biological activity of new mannich base 1-((4-chlorophenyl)(2,5-dioxopyrrolidin-1-YL) methyl) urea",
+    author: "M. R. Abdullahi and S. Rajeswari",
+    articleNumber: "1106B56",
+    abstract:
+      "A new mannich base 1-((4-chlorophenyl)(2,5-dioxopyrrolidin-1-yl)methyl) urea has been synthesized from the condensation of succinimide, 4-chlorobenzaldehyde and urea. The compound has been characterized on the basis of elemental analysis, FTIR, 1HNMR 13C NMR, and Mass spectroscopy. The compound was tested for its antimicrobial activity against Pseudomonas aeruginosa, Staphylococcus aureus, Aspergillus niger  and Candida albicans using agar well diffusion method.",
+    keywords: "Mannich base, succinimide, antimicrobial.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 4,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Assessment of physicochemical properties of clay samples obtained from Ashaka, Potiskum and Tango (NIGERIA) and a commercial bentonite obtained from Kofar Ruwa Market",
+    author: "Abdullahi S. L. and A. A. Audu",
+    articleNumber: "4AF7158",
+    abstract:
+      "Samples of clays suspected to be bentonite were obtained from Ashaka, Tongo and Potiskum, Nigeria and a commercial bentonite obtained from Kofar Ruwa market, Kano State were investigated for some of their physicochemical properties. These determinations were carried out on the wet beneficiated clay samples. The results were compared with results obtained from other studies on bentonite clays. The moisture content, pH, density and swelling power were between 4 to 7%, 7.78 to 9.89, 1.21 to 1.82 g/cm3 and 10 to 13 ml/2 g, respectively. Cation exchange capacity (CEC) was between 61 and 72 meq/100 g; Ca2+ and Na+ were the main exchangeable cations (20.87 to 39.752 and 26.55 to 33.18 meq/100 g, respectively) with calcium ion as the dominant. The chemical composition of the clays indicates high percentages of CaO 1.155 to 14.850 wt%. The silica and alumina contents of the samples were in the ranges of 40.705 to 49.873 wt% and 14.856 to 16.744 wt%, respectively. Fe2O3 content of the samples was in the range of 4.802 to 5.606 wt%. The results of the physicochemical analysis of the samples when compared with the results from other studies on bentonite may suggest that the samples are bentonite clays.",
+    keywords:
+      "Bentonite, montmorillonite, smectite, cation exchange capacity (CEC), swelling power, chemical composition.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 5,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Nanotechnology and implementation in natural products green chemistry processes",
+    author:
+      " M. M. Adeyemi, P. E. Omale, O. A. Olawuyi, A. Ngokat and A. Bhattacharya",
+    articleNumber: "E287F60",
+    abstract:
+      "Nanotechnology, already showing great potential for application in environmental protection has been suggested to be able to provide green and effective alternatives for the management of insect pests in agriculture and post harvest processes. This is in an effort to reduce the hazardous effects of previously used non-biodegradable substances on humans and the environment. The paper will discuss emerging nanotechnologies in antifeedant and pesticide formulations from natural products including novel photodegradable insecticide involving nanoparticles. This will include studies on synthesized nanoparticles used as larvicides, antifeedants, insecticides and pesticides. Nanoscience and nanotechnologies have offered the opportunities to combine organic constituents from natural products with elemental components in order to create complex, innovative and specific material in chemical synthesis for future and immediate use in green technologies.",
+    keywords: "Nanotechnology, green synthesis, natural products.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 6,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Characterization of beach water and its effect on nearby groundwater quality in Eti-osa and Ibeju-lekki local government areas, of Lagos State, Nigeria",
+    author: "V. E. Agbazue and B. U. Ukazu",
+    articleNumber: "2A7A362",
+    abstract:
+      "Groundwater in Eti-Osa and Ibeju-Lekki local government areas of Lagos state were investigated for the effects of beach water influence on adjacent aquifers. Beach water and Borehole samples at distances of 40-50 m and 100-150 m from adjacent beaches in the study areas were taken and analysed for pH, Temperature, Electrical Conductivity, Total Dissolved Solids, Salinity, Ca2+, Mg2+, K+, Na+, HCO3- , NO3-, Cl-SO42-, total hardness and dissolved oxygen. The results were compared with World Health Organisation (WHO) and National Agency for Food and Drug Administration and Control (NAFDAC) standards for water quality. Piper diagram was used to delineate the hydrochemical calfacies of the aquifer and to identify hydrogeochemical changes. The results showed that Cl- dominated in boreholes of both Ibeju-Lekki and Eti-Osa. Sampled boreholes in Eti-Osa showed prevailing dominance of major seawater ions (Na+> Ca2+> K+>: Cl- > SO-) at levels far beyond permissible limits of potable water quality set by WHO and NAFDAC and showed a Ca-SO4 and NaCl water type for the boreholes 40-50 m and 100-115 from the coastline line respectively. Water quality requirements of WHO and NAFDAC are within permissible limits for boreholes in Ibeju-Lekki. Analysed Water samples from Ibeju- Lekkiare characterised as Ca-SO4 water type.Groundwater resources of studied boreholes in Ibeju-Lekki are less influenced by saltwater intrusion from nearby beach, as opposed to Eti-Osa. This is can be attributed to overtly unsustainable exploitation of groundwater resources in Eti-Osa, Which makes groundwater in Eti-Osa unsuitable for domestic purposes.",
+    keywords: "Characterization, groundwater, seawater, intrusion, standard.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 7,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Production of methylester (biodiesel) using melon seed oil as a raw material",
+    author: "Agbo I. U. and Onyezeka E.G.",
+    articleNumber: "044D564",
+    abstract:
+      "The production of the methyl ester was carried out using raw melon seed oil as a raw material through the process of transesterification. The most important variable which influences the transesterification reaction time and conversion were put into consideration.",
+    keywords: "Biodisel, transeterification, methyl ester.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 8,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Sorption kinetics of methylene blue on adsorbents derived from Delonix regia seed pods and Vigna subterranea fruit hulls",
+    author: "L.K. Akinola, Ali Ibrahim and Mohammed Mohammed",
+    articleNumber: "A371B66",
+    abstract:
+      "Kinetics of methylene blue adsorption on adsorbents prepared from Delonix regia seed pods (DRSP) and Vigna subterranea fruit hulls (VSFH) were investigated. Characterization of the adsorbents indicated that the points of zero charge (PZC) of DRSP and VSFH are 5.30 and 4.80 respectively. Optimum removal efficiencies of methylene blue by DRSP and VSFH occurred at pH of 8.00 and 11.00 respectively. For both DRSP and VSFH, equilibrium was attained after 4 hours of agitation time, leading to over 80% removal of methylene blue at ionic strength of 0.4 M and beyond. Among the kinetic models tested, the experimental data fitted well to pseudo- second order model (R2= 0.996 for DRSP and = 0.998 for VSFH), suggesting that the adsorption of methylene blue on DRSP and VSFH involves chemisorption. The study demonstrates that DRSP and VSFH can serve as alternative low-cost adsorbents for removing methylene blue from wastewater.",
+    keywords:
+      "Adsorption, Delonix regia, kinetics, methylene blue, Vigna subterranean",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 9,
+    title: "39th CSN Annual International Conference",
+    topic: "Groundwater qualityin Okitipupa township of Ondo State, Nigeria",
+    author: "Ayedun H. and Ediagbonya T. F.",
+    articleNumber: "3EACE68",
+    abstract:
+      "This study was conducted to assess the quality of groundwater in Okitipupa (Okpa), the head quarter of Okitipupa local government area of Ondo State, Nigeria. Physico-chemical parameters were determined using standard methods while metals were determined using Atomic Absorption Spectrophotometer. The pH range (Mean ± SD) are 8.5- 11.2 (10.1 ± 0.9), the Electrical Conductivity range are 1095- 1932 (1503 ± 211) µSCm-1, the TDS range are 657 – 1159 (902 ± 126) mgL-1, the chloride concentration range are 220- 299 (276 ± 18.5) mgL-1, the Sulphate range are 80.7 – 260 (201 ± 53.5) mgL-1 and Nitrate range 10.1-92 (35.1-19.6) mgL-1. The metal index (MI) was used to assess the level of metal contamination in comparison with WHO maximum acceptable concentration (MAC). The calculated MI values of metals (Fe, Cr, Pb, Cu and Ni) ranged from 1.47- 46.3. The results show that the overall quality of water decreases as MI >1 is a threshold of warning. Improved sanitation and treatment of groundwater meant for drinking is necessary.",
+    keywords: "Groundwater, Okitipupa, metal index, anions, Ondo State.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+  {
+    id: 1,
+    title: "39th CSN Annual International Conference",
+    topic:
+      "Isolation and characterization of 2, 4-dihydroxycinnamic acid from the stem bark of Adina microcephala Delile",
+    author: "S. G. Ayo, J. D. Habila, J. I. Achika and O. O. Akinwande",
+    articleNumber: "47BCC70",
+    abstract:
+      "Adina microcephala which belongs to the family Rubiaceae is widely distributed in Africa. It is used in African traditional medicine for the treatment of diarrhoea and other stomach or digestive tract problems. A microcephala stem bark was collected, identified, dried and pulverized. The pulverized plant material was subjected to Soxhlet extraction using petroleum ether (60-80°C), chloroform, ethyl acetate and methanol. The ethyl acetate extract was subjected to further purification by chromatographic techniques to yield a compound labeled R1 which was then fully characterized as 2, 4-dihydroxycinnamic acid using IR, 1D and 2D NMR spectroscopic techniques.",
+    keywords:
+      "2,4-Dihydroxycinnamic acid, Adina microcephala, Rubiaceae, stem bark.",
+    copywrite:
+      "Copyright © 2022 Author(s) retain the copyright of this article. This article is published under the terms of the Creative Commons Attribution License 4.0",
+    file: "",
+  },
+];
+
+export default proceedings;
+
+/* 
+
+{
+    id: 1,
+    title: "",
+    topic: "",
+    author: "",
+    articleNumber: "",
+    abstract: "",
+    keywords: "",
+    copywrite: "",
+    file: "",
+  },
+
+*/
