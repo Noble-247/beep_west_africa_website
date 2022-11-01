d9e64c8b99e672eecd062f3e2418f38bf8b2b07d
Emmanuel Ekpobimi <noble4special247@gmail.com>
2022-11-01 18:21:47 +0100

    Modefied the JournalCategoryDetails component display journal category details

--- src/components/pages/JournalCategoryDetails.js
+++ src/components/pages/JournalCategoryDetails.js
@@ -1,15 +1,15 @@
 /*TODO: ADD DYNAMIC PAGE TITLE FOR EACH JOURNAL CATEGORY  */
 
-import React, { Fragment } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React from "react";
+import { /* useNavigate, */ useParams } from "react-router-dom";
 import JournalData from "../../Database/allJournals/journals";
 import { Container } from "react-bootstrap";
 
-function JournalCategoryDetails(props) {
+function JournalCategoryDetails() {
   const { journal_category_ID } = useParams();
   //   console.log(journal_category_ID);
 
-  const navigate = useNavigate();
+  /*  const navigate = useNavigate(); */
 
   const thisJournalCategory = JournalData.filter((obj) => {
     return obj.id === parseInt(journal_category_ID);
@@ -17,27 +17,92 @@ function JournalCategoryDetails(props) {
   //   console.log(thisJournalCategory);
 
   return (
-    <Fragment>
-      <Container>
-        <div className='mt-3'>
-          <button onClick={() => navigate(-1)} className='btn btn-dark'>
-            Go Back
-          </button>
-        </div>
-        <h2>JournalCategoryDetails - {journal_category_ID}</h2>
-        <h1>
-          {thisJournalCategory.map((journal) => {
-            return journal.title;
-          })}
-        </h1>
-        <h2>
-          {thisJournalCategory.map((journal) => {
-            return journal.category;
-          })}
-        </h2>
-      </Container>
-    </Fragment>
+    <Container fluid className='p-0'>
+      {thisJournalCategory.map((category) => {
+        return (
+          <div>
+            <section className='bg-dark text-white py-5' key={category.id}>
+              <Container>
+                <h1 className='text-center h2 text-uppercase'>
+                  <span>{category.title}</span>
+                  <span>{category.category}</span>
+                </h1>
+                <section className='text-center'>
+                  <p className='d-inline-block me-4 pe-3 border-end'>
+                    Language: {category.language}
+                  </p>
+                  <p className='d-inline-block me-4 pe-3 border-end'>
+                    ISSN: {category.ISSN}
+                  </p>
+                  <p className='d-inline-block me-4 pe-3 border-end'>
+                    Start Year: {category.startYear}
+                  </p>
+                  <p className='d-inline-block me-4 pe-3'>
+                    Articles Published: {category.numberOfArticlesPublished}
+                  </p>
+                </section>
+              </Container>
+            </section>
+            <section>
+              {category.articlesInThisCategory.map((article) => {
+                return (
+                  <div key={article.id}>
+                    <Container>
+                      <h3>{article.topic}</h3>
+                    </Container>
+                  </div>
+                );
+              })}
+            </section>
+          </div>
+        );
+      })}
+    </Container>
   );
 }
 
 export default JournalCategoryDetails;
+
+/* 
+
+{thisJournalCategory.map((category) => {
+          return (
+            <Container key={category.id}>
+              <h1 className='text-center h2 text-uppercase'>
+                <span>{category.title}</span>
+                <span>{category.category}</span>
+              </h1>
+              <section className='text-center'>
+                <p className='d-inline-block me-4 pe-3 border-end'>
+                  Language: {category.language}
+                </p>
+                <p className='d-inline-block me-4 pe-3 border-end'>
+                  ISSN: {category.ISSN}
+                </p>
+                <p className='d-inline-block me-4 pe-3 border-end'>
+                  Start Year: {category.startYear}
+                </p>
+                <p className='d-inline-block me-4 pe-3'>
+                  Articles Published: {category.numberOfArticlesPublished}
+                </p>
+              </section>
+              <section>
+                {category.articlesInThisCategory.map((article) => {
+                  return (
+                    <div key={article.id}>
+                      <h3>{article.topic}</h3>
+                    </div>
+                  );
+                })}
+              </section>
+            </Container>
+          );
+        })}
+
+*/
+
+/*   <div className='mt-3'>
+          <button onClick={() => navigate(-1)} className='btn btn-dark'>
+            Go Back
+          </button> p-3
+        </div> */
