5a15f73ccf93f7073c977d657c2758a0f233fac6
Emmanuel Ekpobimi <noble4special247@gmail.com>
2022-12-23 15:02:39 +0100

    Finished building the ContactUs Page

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,13 +1,124 @@
 import React, { Fragment } from "react";
-import { Container } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import useTitle from "../customHooks/useTitle";
 
 function ContactUs() {
   useTitle("Beep West Africa | Contact Us");
+
+  const [formData, setFormData] = React.useState({
+    selectDepartment: "",
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  //console.log(formData);
+
+  function updateFormData(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(formData);
+  }
+
+  // console.log(formData.selectDepartment);
+
   return (
     <Fragment>
       <Container>
-        <h1>ContactUs</h1>
+        <Row className='mt-5 justify-content-center'>
+          <Col md={6}>
+            <Card className='shadow-lg'>
+              <Card.Header className='h3 text-center'>
+                Registration Form
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>
+                  Fill and submit this form, and we will contact you ASAP
+                </Card.Title>
+                <Form>
+                  <Form.Group className='my-3'>
+                    <Form.Label>Select Department</Form.Label>
+                    <Form.Select
+                      aria-label='select department'
+                      value={formData.selectDepartment}
+                      name='selectDepartment'
+                      onChange={updateFormData}
+                    >
+                      <option disabled value='instruction'>
+                        ---Choose Department---
+                      </option>
+                      <option value='help-desk'>Help Desk</option>
+                      <option value='accounts'>Accounts</option>
+                      <option value='ethics-and-compliance'>
+                        Ethics and Compliance
+                      </option>
+                      <option value='editorial-office'>Editorial Office</option>
+                    </Form.Select>
+                  </Form.Group>
+
+                  <Form.Group className='mb-3'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type='text'
+                      placeholder='type your name here'
+                      value={formData.name}
+                      name='name'
+                      onChange={updateFormData}
+                      required='required'
+                    ></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group className='mb-3'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type='email'
+                      placeholder='type your name here'
+                      value={formData.email}
+                      name='email'
+                      onChange={updateFormData}
+                      required='required'
+                    ></Form.Control>
+                    <Form.Text className='text-muted'>
+                      We'll never share your email with anyone else
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className='mb-3'>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                      as='textarea'
+                      rows={3}
+                      placeholder='type your message here'
+                      name='message'
+                      value={formData.message}
+                      onChange={updateFormData}
+                    ></Form.Control>
+                  </Form.Group>
+                </Form>
+                <div className='d-flex justify-content-around'>
+                  <Button
+                    onClick={handleSubmit}
+                    type='submit'
+                    className='d-block'
+                    variant='success'
+                  >
+                    Send
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </Fragment>
   );
