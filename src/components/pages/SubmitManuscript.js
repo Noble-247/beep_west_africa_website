4fd3c272161e96a89cba30ea814d23e0c9d231c2
Emmanuel Ekpobimi <noble4special247@gmail.com>
2022-12-24 20:09:27 +0100

    Moved the three Forms in the SubmitManuscript component to their respective components (ReviewerLoginFormForManuscript, EditorLoginFormForManuscript and AuthorLoginFormForManuscript)

--- src/components/pages/SubmitManuscript.js
+++ src/components/pages/SubmitManuscript.js
@@ -1,42 +1,12 @@
 import React, { Fragment } from "react";
-import {
-  Button,
-  Card,
-  Col,
-  Container,
-  Form,
-  Row,
-  Tab,
-  Tabs,
-} from "react-bootstrap";
+import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
 import useTitle from "../customHooks/useTitle";
+import AuthorLoginFormForManuscript from "../forms/AuthorLoginFormForManuscript";
+import EditorLoginFormForManuscript from "../forms/EditorLoginFormForManuscript";
+import ReviewerLoginFormForManuscript from "../forms/ReviewerLoginFormForManuscript";
 
 function SubmitManuscript() {
   useTitle("Beep West Africa | Submit Manuscript");
-
-  const [formData, setFormData] = React.useState({
-    userName: "",
-    password: "",
-  });
-
-  function updateFormData(event) {
-    const { name, value } = event.target;
-    setFormData((previousFormData) => {
-      return {
-        ...previousFormData,
-        [name]: value,
-      };
-    });
-  }
-
-  /*  function clearFormData(event) {
-    setFormData({ userName: "", password: "" });
-  }
- */
-  function handleRegister(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
 
   return (
     <Fragment>
@@ -54,132 +24,21 @@ function SubmitManuscript() {
                 title='AUTHOR'
                 style={{ borderRadius: "0px" }}
               >
-                <div>
-                  <Card className='shadow-lg'>
-                    <Card.Header className='h3 text-center'>
-                      AUTHOR'S LOGIN
-                    </Card.Header>
-                    <Card.Body>
-                      <Form>
-                        <Form.Group className='mb-3'>
-                          <Form.Label>Username</Form.Label>
-                          <Form.Control
-                            type='text'
-                            placeholder='Enter Username'
-                            name='userName'
-                            value={formData.userName}
-                            onChange={updateFormData}
-                          ></Form.Control>
-                        </Form.Group>
-                        <Form.Group className='mb-3'>
-                          <Form.Label>Password</Form.Label>
-                          <Form.Control
-                            type='password'
-                            placeholder='Enter Password'
-                            name='password'
-                            value={formData.password}
-                            onChange={updateFormData}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Form>
-                      <div className='d-flex justify-content-center'>
-                        <Button
-                          onClick={handleRegister}
-                          type='submit'
-                          className='d-block btn btn-dark text-primary fw-semibold w-100'
-                        >
-                          SIGN IN
-                        </Button>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </div>
+                <section>
+                  <AuthorLoginFormForManuscript />
+                </section>
               </Tab>
 
               <Tab eventKey='EDITOR' title='EDITOR'>
-                <div>
-                  <Card className='shadow-lg'>
-                    <Card.Header className='h3 text-center'>
-                      EDITOR'S LOGIN
-                    </Card.Header>
-                    <Card.Body>
-                      <Form>
-                        <Form.Group className='mb-3'>
-                          <Form.Label>Username</Form.Label>
-                          <Form.Control
-                            type='text'
-                            placeholder='Enter Username'
-                            name='userName'
-                            value={formData.userName}
-                            onChange={updateFormData}
-                          ></Form.Control>
-                        </Form.Group>
-                        <Form.Group className='mb-3'>
-                          <Form.Label>Password</Form.Label>
-                          <Form.Control
-                            type='password'
-                            placeholder='Enter Password'
-                            name='password'
-                            value={formData.password}
-                            onChange={updateFormData}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Form>
-                      <div className='d-flex justify-content-center'>
-                        <Button
-                          onClick={handleRegister}
-                          type='submit'
-                          className='d-block btn btn-dark text-primary fw-semibold w-100'
-                        >
-                          SIGN IN
-                        </Button>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </div>
+                <section>
+                  <EditorLoginFormForManuscript />
+                </section>
               </Tab>
 
               <Tab eventKey='REVIEWER' title='REVIEWER'>
-                <div>
-                  <Card className='shadow-lg'>
-                    <Card.Header className='h3 text-center'>
-                      REVIEWER'S LOGIN
-                    </Card.Header>
-                    <Card.Body>
-                      <Form>
-                        <Form.Group className='mb-3'>
-                          <Form.Label>Username</Form.Label>
-                          <Form.Control
-                            type='text'
-                            placeholder='Enter Username'
-                            name='userName'
-                            value={formData.userName}
-                            onChange={updateFormData}
-                          ></Form.Control>
-                        </Form.Group>
-                        <Form.Group className='mb-3'>
-                          <Form.Label>Password</Form.Label>
-                          <Form.Control
-                            type='password'
-                            placeholder='Enter Password'
-                            name='password'
-                            value={formData.password}
-                            onChange={updateFormData}
-                          ></Form.Control>
-                        </Form.Group>
-                      </Form>
-                      <div className='d-flex justify-content-center'>
-                        <Button
-                          onClick={handleRegister}
-                          type='submit'
-                          className='d-block btn btn-dark text-primary fw-semibold w-100'
-                        >
-                          SIGN IN
-                        </Button>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </div>
+                <section>
+                  <ReviewerLoginFormForManuscript />
+                </section>
               </Tab>
             </Tabs>
           </Col>
