37e2a6d8a2b73172f940f201b7f394800b3b7998
Emmanuel Ekpobimi <noble4special247@gmail.com>
2023-03-01 18:46:19 +0100

    CallForPapers component was renamed "GeneralNews"

--- src/components/pages/GeneralNews.js
+++ src/components/pages/GeneralNews.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from "react";
 import { Container } from "react-bootstrap";
 import useTitle from "../customHooks/useTitle";
+import Spinner from "../utilities/Spinner";
 
-function CallForPaper() {
+function GeneralNews() {
   useTitle("Beep West Africa | Call For Paper");
   return (
     <Fragment>
-      <Container>
-        <h1>CallForPaper</h1>
+      <Container className='mt-5'>
+        <Spinner />
       </Container>
     </Fragment>
   );
 }
 
-export default CallForPaper;
+export default GeneralNews;
