e8e9a9ec03fcef2f4ac0d104d979ea4f2c0bcf67
Emmanuel Ekpobimi <noble4special247@gmail.com>
2023-03-06 13:07:07 +0100

    Added GeneralNews Component

--- src/components/pages/GeneralNews.js
+++ src/components/pages/GeneralNews.js
@@ -1,14 +1,81 @@
-import React, { Fragment } from "react";
-import { Container } from "react-bootstrap";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+import { Col, Container, Row } from "react-bootstrap";
 import useTitle from "../customHooks/useTitle";
 import Spinner from "../utilities/Spinner";
 
 function GeneralNews() {
-  useTitle("Beep West Africa | Call For Paper");
+  useTitle("Beep West Africa | General News");
+
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [categoryHeading, setCategoryHeading] = useState("");
+
+  function getScienceNews() {
+    const url = "https://inshorts.deta.dev/news?category=science";
+
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        setCategoryHeading(data.category);
+        setNews(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    getScienceNews();
+  }, []);
+
   return (
     <Fragment>
       <Container className='mt-5'>
-        <Spinner />
+        <div>{loading && <Spinner />}</div>
+        <div className='text-center'>
+          <h2>
+            <span>{categoryHeading && categoryHeading.toUpperCase()}</span>{" "}
+            <span>{categoryHeading && "NEWS FROM CREDIBLE SOURCES"}</span>
+          </h2>
+        </div>
+        <div>
+          {news &&
+            news.map((story) => (
+              <Row
+                key={story.id}
+                className='align-items-center justify-content-center'
+              >
+                <Col md={10} className='bg-black my-2 p-3'>
+                  <h5 className='text-primary'>{story.title}</h5>
+                  <p className='text-white'>{story.content}</p>
+                  <a
+                    href={story.readMoreUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='btn btn-outline-light'
+                  >
+                    Read More
+                  </a>
+                  <h6 className='text-primary mt-3 mb-0'>
+                    Author: {story.author}
+                  </h6>
+                  <small className='text-primary'>
+                    {story.date}, {story.time}
+                  </small>
+                </Col>
+              </Row>
+            ))}
+        </div>
+        <div className='text-center text-danger my-5'>
+          {error && <h1>Sorry, A {error} Occured </h1>}
+        </div>
       </Container>
     </Fragment>
   );
