2da75e4fdde700bb02cd06aeada1bd843161ab87
Emmanuel Ekpobimi <noble4special247@gmail.com>
2023-03-16 14:30:32 +0100

    Finished building the SiteUnderConstruction page, added World-Map svg, TypeWriterAnimation and CountDownTimer utilities

---
+++ src/components/pages/SiteUnderConstruction.js
@@ -0,0 +1,28 @@
+import CountDownTimer from "../utilities/CountDownTimer";
+import TypeWriterAnimation from "../utilities/TypeWriterAnimation";
+import { Button, Container } from "react-bootstrap";
+
+function SiteUnderConstruction(props) {
+  return (
+    <div className='site-under-maintenance'>
+      <Container>
+        <CountDownTimer targetDate='2023-12-31T00:00:00' />
+        <TypeWriterAnimation />
+        <p>
+          BEEP WESTAFRICA is a peer-reviewed International Journal Research
+          Organization of high editorial quality. BEEP is also a universally
+          accessible online journal. The journal publishes researched articles
+          in all areas of science, art, management and technology
+        </p>
+        <Button
+          className='btn btn-secondary d-block mx-auto mt-3 fw-bold'
+          onClick={() => props.showMainContent()}
+        >
+          CLICK TO EXPLORE THE WEBSITE
+        </Button>
+      </Container>
+    </div>
+  );
+}
+
+export default SiteUnderConstruction;
